60e2de84cbe3bca3724cb7ec43c9769eb4e09195
Gilberto Hernandez <teti.programmer@gmail.com>
2021-08-02 12:16:07 -0300

    Added functionality to timer to connect to the outside world

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const pauseBtn = document.querySelector('#pause');
 const inputTime = document.querySelector('#duration');
 
 class Timer {
-  constructor(timeInput, startButton, pauseButton) {
+  constructor(timeInput, startButton, pauseButton, callbacks) {
     this.timeInput = timeInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
+    } else {
+      console.log('no callbacks for u :(');
+    }
 
     this.timeInput.addEventListener('change', this.onDurationChange);
     this.startButton.addEventListener('click', this.start);
@@ -14,6 +21,9 @@ class Timer {
   }
 
   start = () => {
+    if (this.onStart) {
+      this.onStart();
+    }
     this.tick();
     this.interval = setInterval(this.tick, 1000);
   };
@@ -28,8 +38,36 @@ class Timer {
   }
 
   tick = () => {
-    console.log('tick');
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
+      if (this.onTick) {
+        this.onTick();
+      }
+    }
   };
+
+  get timeRemaining() {
+    return parseFloat(this.timeInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.timeInput.value = time;
+  }
 }
 
-const timer = new Timer(inputTime, startBtn, pauseBtn);
+const timer = new Timer(inputTime, startBtn, pauseBtn, {
+  onStart() {
+    console.log("OnStart executed: Say something, I'm giving up on you 🎵");
+  },
+  onTick() {
+    console.log('OnTick executed: Tick tick');
+  },
+  onComplete() {
+    console.log('OnComplete executed: Finishshshhs');
+  },
+});
